a626d6472c89ced6fd703d358160936a962cd064
TheFloydman <danielwellsfloyd@gmail.com>
2020-05-11 20:30:36 -0700

    Add files via upload

--- mystlibrarywordcreator/mystlibrarywordcreator.js
+++ mystlibrarywordcreator/mystlibrarywordcreator.js
@@ -130,8 +130,13 @@ function save() {
 		}
 	});
 	
-	var data = "[\r\n  {\r\n    \"word\": \"" + word + "\",\r\n    \"arcs\": " + JSON.stringify(idArray).replace(/,/g, ", ") + ",\r\n    \"colors\": " + JSON.stringify(colorArray).replace(/,/g, ", ") + "\r\n  }\r\n]";
-	var jsonData = data;
+	var jsonData = `[
+  {
+    "word": "${word}",
+    "arcs": ${JSON.stringify(idArray).replace(/,/g, ", ")},
+    "colors": ${JSON.stringify(colorArray).replace(/,/g, ", ")}
+  }
+]`;
 
 	var a = document.createElement("a");
     var file = new Blob([jsonData], {type: "text/plain"});
